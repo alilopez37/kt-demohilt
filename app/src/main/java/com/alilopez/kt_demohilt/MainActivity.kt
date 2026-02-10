af0655b8d1414e65a5843643d2c303fcc4000240
alilopez <ali.lopez37@gmail.com>
2026-02-09 22:22:16 -0600

    Initial commit

---
+++ app/src/main/java/com/alilopez/kt_demohilt/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.alilopez.kt_demohilt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alilopez.kt_demohilt.core.ui.theme.AppTheme
+import com.alilopez.kt_demohilt.features.feature01.presentation.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/alilopez/kt_demohilt/features/feature01/presentation/HomeScreen.kt
@@ -0,0 +1,27 @@
+package com.alilopez.kt_demohilt.features.feature01.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+
+    ) {
+        Text("Home Screen",
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer)
+    }
+}
